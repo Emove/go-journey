7d361f68e557b10b8d433e6bfcddfee0071f907d
Emove@668466 <1019291811@qq.com>
2021-09-26 17:33:56 +0800

    middle_node, remove_nth_from_end

---
+++ leetcode/code/structure/list_node.go
@@ -0,0 +1,27 @@
+package structure
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func NewListNode(val int) *ListNode {
+	return &ListNode{
+		Val: val,
+	}
+}
+
+func BuildListNode(nums []int) *ListNode {
+	if len(nums) == 0 {
+		return &ListNode{}
+	}
+	head := NewListNode(nums[0])
+	point := head
+	length := len(nums)
+	for i := 1; i < length; i++ {
+		node := NewListNode(nums[i])
+		point.Next = node
+		point = node
+	}
+	return head
+}
