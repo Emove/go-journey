7e7ac379207ba43ff2698c893e1bb8529a5fe7b9
Emove@668466 <1019291811@qq.com>
2022-03-05 15:12:20 +0800

    longest uncommon subsequence_1 521

---
+++ leetcode/code/521_longest_uncommon_subsequence_1/solution.go
@@ -0,0 +1,13 @@
+package _21_longest_uncommon_subsequence_1
+
+func findLUSlength(a string, b string) int {
+	if a != b {
+		n := len(a)
+		m := len(b)
+		if n > m {
+			return n
+		}
+		return m
+	}
+	return -1
+}
