2488f7060bc3493ec8059762575a5569d0c2f986
Emove@668466 <1019291811@qq.com>
2022-09-11 17:06:19 +0800

    context

--- blemish/context/why.go
+++ blemish/context/why.go
@@ -1,3 +1,5 @@
 package context
 
-// it is morbidity that ubiquity of context in golang project though it is unessential indeed
+// 1、it is morbidity that ubiquity of context though it is unessential indeed.
+
+// 2、in some situation, context is used to pass through some parameter implicitly, i don't think it is a good choice.
