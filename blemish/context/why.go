54a463a2221df0955a27c5f8b75bbf1277ae759b
Emove@668466 <1019291811@qq.com>
2022-09-11 17:07:10 +0800

    context

--- blemish/context/why.go
+++ blemish/context/why.go
@@ -2,4 +2,4 @@ package context
 
 // 1、it is morbidity that ubiquity of context though it is unessential indeed.
 
-// 2、in some situation, context is used to pass through some parameter implicitly, i don't think it is a good choice.
+// 2、in some situations, context is used to pass through parameters implicitly, i don't think it is a good choice.
