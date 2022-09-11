35bea480e04a7304392984994783d4cb67b9a55d
Emove@668466 <1019291811@qq.com>
2022-09-11 15:04:41 +0800

    correct words

--- blemish/interface/example.go
+++ blemish/interface/example.go
@@ -9,7 +9,7 @@ type Buffer struct {
 // 但是在无意间，我实现了 io.Writer
 // 若我这时，想要查看那些地方引用了 Write 方法
 // 会找出一堆 io.Writer.Write 方法的引用，而无法准确的找出对于 Buffer.Write 的引用
-// 这时隐式实现机制的一个弊端，而非IDE设计不完善的锅
+// 这是隐式实现机制的一个弊端，而非IDE设计不完善的锅
 func (buf *Buffer) Write(b []byte) (n int, err error) {
 	buf.b = append(buf.b, b...)
 	return len(b), nil
