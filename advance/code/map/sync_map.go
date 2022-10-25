3ccf9bb337879b526c5d19f28766d064e7387c3a
emove <emove.lam@qq.com>
2022-10-25 22:33:49 +0800

    [test] test sync.Map#LoadOrStore

--- advance/code/map/sync_map.go
+++ advance/code/map/sync_map.go
@@ -16,4 +16,26 @@ func LoadOrStore() {
 	store, loaded = m.LoadOrStore("second_key", "second_value")
 	fmt.Printf("loaded: %v, value: %v\n", loaded, store)
 
+	// invalid
+	type s struct {
+		v string
+	}
+	var v *s
+	store, loaded = m.LoadOrStore("third_key", v)
+	fmt.Printf("loaded: %v, value: %v\n", loaded, store)
+	v = &s{v: "third_value"}
+	store, loaded = m.Load("third_key")
+	fmt.Printf("loaded: %v, value: %v\n", loaded, store)
+
+	// work
+	type wrapper struct {
+		delegate *s
+	}
+	w := &wrapper{}
+	store, loaded = m.LoadOrStore("forth_key", w)
+	fmt.Printf("loaded: %v, value: %v\n", loaded, store)
+	w.delegate = v
+	store, loaded = m.Load("forth_key")
+	fmt.Printf("loaded: %v, value: %v\n", loaded, store)
+
 }
