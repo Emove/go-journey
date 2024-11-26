b28178a89efe8eae3b6cbffc18e2f0293d5ac27b
Emov <emove.lam@qq.com>
2024-11-26 19:54:09 +0800

    add math test

--- basis/bulit_in/math/round.go
+++ basis/bulit_in/math/round.go
@@ -6,6 +6,14 @@ import (
 )
 
 func Round() {
-	fmt.Println(uint32(float64(1.5)))
+	//fmt.Println(uint32(float64(1.5)))
 	fmt.Println(math.Round(0.5))
+}
+
+func Div(num int) {
+	fmt.Println(float64(num / 2))
+}
+
+func Ceil(num int) {
+	fmt.Println(math.Ceil(float64(num) / 2))
 }
--- basis/bulit_in/math/round_test.go
+++ basis/bulit_in/math/round_test.go
@@ -5,3 +5,11 @@ import "testing"
 func TestRound(t *testing.T) {
 	Round()
 }
+
+func TestDiv(t *testing.T) {
+	Div(5)
+}
+
+func TestCeil(t *testing.T) {
+	Ceil(1801)
+}
